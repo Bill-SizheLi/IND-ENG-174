c78d9c5ec8ee9e6c25d7a1fe4cf7e5f38e2f198b
Bill-SizheLi <sizheli@berkeley.edu>
2024-12-15 15:59:06 -0800

    fix missing arg

--- Part_1_IcuQueue/IcuQueueMain.py
+++ Part_1_IcuQueue/IcuQueueMain.py
@@ -1,3 +1,7 @@
+import sys
+
+project_root = '/Users/sizheli/Desktop/INDENG_174/IND-ENG-174'
+sys.path.append(project_root)
 from Part_1_IcuQueue.DepartureProcessWithDPQandReservedBeds import simultaneously_return
 import numpy as np
 import matplotlib.pyplot as plt
@@ -85,20 +89,6 @@ def plot_average_severity_distribution(grouped_avg, bin_size = 24, save_path = N
 
 
 
-# Create output directories
-output_dir = "Part_1_IcuQueue"
-figures_dir = os.path.join(output_dir, "figures")
-os.makedirs(figures_dir, exist_ok=True)
-
-# Save the plot to the figures folder
-plot_path = os.path.join(figures_dir, "DPQ+reserved.png") 
-
-grouped_avg = run_multiple_simulations(num_simulations=100, bin_size=24)
-plot_average_severity_distribution(grouped_avg, bin_size=24, save_path=plot_path)
-
-
-
-
 
 
 # calculate the average penalty and CI of penalties
@@ -144,19 +134,19 @@ if __name__ == "__main__":
 
     average_penalty, penalty_list = penalty_average_1(sample_size)
 
-# Create a penalties subdirectory
-penalties_dir = os.path.join(output_dir, "penalties")
-os.makedirs(penalties_dir, exist_ok=True)
+    # Create a penalties subdirectory
+    penalties_dir = os.path.join(output_dir, "penalties")
+    os.makedirs(penalties_dir, exist_ok=True)
 
-# Save the average penalty to a file
-penalty_file_name = "DPQ+reserved.txt" 
-penalty_path = os.path.join(penalties_dir, penalty_file_name)
+    # Save the average penalty to a file
+    penalty_file_name = "DPQ+reserved.txt" 
+    penalty_path = os.path.join(penalties_dir, penalty_file_name)
 
-with open(penalty_path, "w") as f:
-    f.write(f"Penalty 1(Average): {average_penalty}\n")
-    f.write(f"95% Confidence Interval:: {calculate_confidence_interval()}\n")
+    with open(penalty_path, "w") as f:
+        f.write(f"Penalty 1(Average): {average_penalty}\n")
+        f.write(f"95% Confidence Interval:: {calculate_confidence_interval(penalty_list)}\n")
 
-print(f"Penalty result saved to {penalty_path}")
+    print(f"Penalty result saved to {penalty_path}")
 
-print("Average Penalty:", average_penalty)
-print("95% Confidence Interval:", calculate_confidence_interval(penalty_list))
+    print("Average Penalty:", average_penalty)
+    print("95% Confidence Interval:", calculate_confidence_interval(penalty_list))
